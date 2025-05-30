6b01aabebcd26c67f16f96b19d8bb95cc1b39f82
Alexander Koptev <askoptevwork@gmail.com>
2025-05-30 07:20:48 +0300

    11.9. Домашнее задание - Модульность и библиотеки

---
+++ 11-sort-by/index.ts
@@ -0,0 +1,26 @@
+import pkg from 'sort-by';
+import { sortBy } from 'sort-by';
+
+sortBy()
+const users = [
+  {
+    id: 7,
+    name: 'Foo',
+    age: '34',
+    email: { primary: 'foo@email.com' },
+  },
+  {
+    id: 3,
+    name: 'Baz',
+    age: '67',
+    email: { primary: 'baz@email.com' },
+  },
+  {
+    id: 4,
+    name: 'Bar',
+    age: '67',
+    email: { primary: 'bar@email.com' },
+  },
+];
+
+users.sort(sortBy())
